51b1ad9fe54f512e7688b3ff8ff331fa21a0b3c0
kooreung <crisishyun@gmail.com>
2024-05-07 11:43:02 +0900

    Project Setting

---
+++ src/main/java/com/project1_prac/Project1PracApplication.java
@@ -0,0 +1,13 @@
+package com.project1_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1PracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project1PracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project1_prac/Project1PracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1PracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
